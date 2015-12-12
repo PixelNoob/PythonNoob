f37fd24be1e01a5335176b9927cc35750e829a2f
PixelNoob <jesuschitty7@hotmail.com>
2015-12-12 00:51:59 -0300

    Create working_hours.py

---
+++ working_hours.py
@@ -0,0 +1,9 @@
+totalpay = raw_input ("How much did you get paid this month?")
+total_hours = 176
+pay_per_hour = float(totalpay) / total_hours
+print "ARS per hour", pay_per_hour
+print "USD per Hour", pay_per_hour / 15
+if pay_per_hour < 124:
+    print "pobre"
+if pay_per_hour > 125:
+    print "Rico
