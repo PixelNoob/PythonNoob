4b04b9dd3cf488b094baef37da17a2dec80401bf
PixelNoob <jesuschitty7@hotmail.com>
2015-12-14 10:59:27 -0300

    Updating conditionals
    Adding <= and >= conditionals

--- working_hours.py
+++ working_hours.py
@@ -3,7 +3,7 @@ total_hours = 176
 pay_per_hour = float(totalpay) / total_hours
 print "ARS per hour", pay_per_hour
 print "USD per Hour", pay_per_hour / 15
-if pay_per_hour < 124:
+if pay_per_hour <= 124:
     print "pobre"
-if pay_per_hour > 125:
+if pay_per_hour >= 125:
     print "Rico"
