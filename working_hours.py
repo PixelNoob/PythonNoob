0e637579b44ff4604d2ca615ee0703e256591fe5
PixelNoob <jesuschitty7@hotmail.com>
2015-12-15 18:36:33 -0300

    updating
    adding the result of the total pay in the print messages

--- working_hours.py
+++ working_hours.py
@@ -1,6 +1,7 @@
 totalpay = raw_input ("How much did you get paid this month?")
 total_hours = 176
 pay_per_hour = float(totalpay) / total_hours
+print "your earned", totalpay, "this month"
 print "ARS per hour", pay_per_hour
 print "USD per Hour", pay_per_hour / 15
 if pay_per_hour <= 124:
