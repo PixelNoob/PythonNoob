c08c0a3fea2e73b0fed51db154cdcd9fd647eeb4
PixelNoob <jesuschitty7@hotmail.com>
2016-08-17 16:26:50 -0300

    Update
    Update decimals

--- pricefeed.py
+++ pricefeed.py
@@ -9,6 +9,6 @@ BTC = result['USDT_BTC']['last']
 Steem = result['BTC_STEEM']['last']
 SBD_USD = float(SBD) * float(BTC)
 
-print "BTC", BTC
+print "BTC", round(float(BTC),2)
 print "Steem", Steem
-print "SBD", '{0:f}'.format(SBD_USD)
+print "SBD", '{0:.2f}'.format(SBD_USD)
