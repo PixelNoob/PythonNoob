087d830c28b31b2439c4b5c5446fbf6b9e8d8df6
J. Chitty <jesuschitty7@hotmail.com>
2021-10-15 09:02:16 -0400

    Update pricefeed.py
    Poloniex still lives lol

--- scripts/pricefeed.py
+++ scripts/pricefeed.py
@@ -4,11 +4,8 @@ import json
 url = urlopen('https://poloniex.com/public?command=returnTicker').read()
 result = json.loads(url)
 
-SBD = result['BTC_SBD']['last']
 BTC = result['USDT_BTC']['last']
 Steem = result['BTC_STEEM']['last']
-SBD_USD = float(SBD) * float(BTC)
 
 print "BTC", round(float(BTC),2)
 print "Steem", Steem
-print "SBD", '{0:.2f}'.format(SBD_USD)
